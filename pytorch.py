579da9ff43935ae44c12d4420d4bba88f5964cd3
Nripdev Singh <126868124+nripsaggu10@users.noreply.github.com>
2023-12-15 20:23:11 +0530

    Create pytorch.py

---
+++ pytorch.py
@@ -0,0 +1,17 @@
+# machine_learning_model/train_model.py
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+
+# Define your PyTorch model
+class PlantDiseaseModel(nn.Module):
+    # Your model definition here
+
+# Define a dataset class
+class PlantDiseaseDataset(Dataset):
+    # Your dataset definition here
+
+# Training code
+def train_model():
+    # Your training code here
